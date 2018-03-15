0c18f7608d430e3634048882558758e95e95906f
kurama17 <36087693+kurama17@users.noreply.github.com>
2018-03-15 14:45:22 +0100

    Add files via upload

---
+++ PINAC/Claude.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PINAC
+{
+    class Claude
+    {
+        private const string soapEnv = @"<?xml version=""1.0"" encoding=""utf-8""?>
+                                      <soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">
+                                            <soap:Body>
+                                                  {0}
+                                            </soap:Body>
+                                      </soap:Envelope> ";
+
+        // WebService appel au WebClient
+        private static Task<string> CallWebClient(string request)
+        {
+            WebClient client = new WebClient();
+            client.Headers["Content-Type"] = "text/xml; charset=utf-8";
+            client.Encoding = System.Text.Encoding.UTF8;
+            Task<string> getStringTask = client.UploadStringTaskAsync("http://46.105.1.31/Claude/ClaudeService.asmx", "POST", request);
+
+            return getStringTask;
+        }
+
+        // WebService aboutClaude
+        //public static Task<string> aboutClaude()
+        //{
+        //    string soapRequest = string.Format(soapEnv, "<aboutClaude />");
+        //    return CallWebClient(soapRequest);
+        //}
+
+        // WebService userLogin
+        public static Task<string> userLogin(string login, string password)
+        {
+            string fonction = string.Format(@"<userLogin>
+                                                <login>{0}</login>
+                                                <password>{1}</password>
+                                            </userLogin>", login, password);
+
+            string soapRequest = string.Format(soapEnv, fonction);
+            return CallWebClient(soapRequest);
+        }
+    }
+}
---
+++ PINAC/loginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PINAC
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class loginPage : ContentPage
+	{
+		public loginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            string login = this.entryLogin.Text;
+            string password = this.entryPassword.Text;
+
+            Task<string> getStringTask = Claude.userLogin(login, password);
+            string responseSOAP = await getStringTask;
+
+            if(login == "house" && password == "aaaaaa")
+                await Navigation.PushModalAsync(new ListConsultation(), true);
+            else
+                await DisplayAlert("Réponse SOAP", responseSOAP, "OK");
+        }
+    }
+}
