29aea0d18ce1b2be53d3064e51426cb5641e90e1
Azalmut <Azalmut@vivaldi.net>
2018-03-15 11:45:22 +0100

    Add files via upload

--- PINAC/Patient.cs
+++ PINAC/Patient.cs
@@ -6,5 +6,16 @@ namespace PINAC
 {
     class Patient
     {
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public int sexe { get; set; }
+        public int age { get; set; }
+        public string adresse { get; set; }
+        public string telephone { get; set; }
+
+        public List<string> antePerso { get; set; }
+        public List<string> anteFamil { get; set; }
+        public List<string> Risques { get; set; }
+
     }
 }
