9f0ef36d58f8535768ca5db62c84fade1c455494
Azalmut <Azalmut@vivaldi.net>
2018-03-16 12:26:38 +0100

    Add files via upload

--- PINAC/Agenda.xaml.cs
+++ PINAC/Agenda.xaml.cs
@@ -9,14 +9,15 @@ using Xamarin.Forms.Xaml;
 
 namespace PINAC
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Agenda : ContentPage
-	{
-		public Agenda()
-		{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Agenda : ContentPage
+    {
+        private Patient patient;
+        public Agenda()
+        {
             InitializeComponent();
-            
-            // chargerListeRDV(Convert.ToDateTime("16/03/2018"));
+
+             chargerListeRDV("16/03/2018");
         }
 
         //async private void chargerListeDossier(string id)
@@ -54,42 +55,45 @@ namespace PINAC
         //    }
         //}
 
-        async private void chargerListeRDV(DateTime date)
+        async private void chargerListeRDV(string date)
         {
             Task<string> getStringTask = Claude.getRDV(date);
             string responseSOAP = await getStringTask;
 
             XElement xml = XElement.Parse(responseSOAP);
 
-            //if (xml.Descendants("Success").FirstOrDefault().Value == "true")
-            //{
-            //    string data = xml.Descendants("Data").FirstOrDefault().Value;
-            //    XElement xmlData = XElement.Parse(data);
-            //    var query = from item in xmlData.Descendants("PAT_Consult")
-            //                select new DossierPatient
-            //                {
-            //                    idDossier = item.Element("id").Value,
-            //                    idPatient = item.Element("PatientId").Value,
-            //                    dateConsultation = Convert.ToDateTime(item.Element("DateConsult").Value),
-            //                    motif = item.Element("Motif").Value,
-            //                    examClinique = item.Element("ExamClinique").Value,
-            //                    diagnostic = item.Element("Diagnostic").Value
-            //                };
+            if (xml.Descendants("Success").FirstOrDefault().Value == "true")
+            {
+                string data = xml.Descendants("Data").FirstOrDefault().Value;
+                XElement xmlData = XElement.Parse(data);
+                var query = from item in xmlData.Descendants("AGD_Rdv")
+                            select new RDV
+                            {
+                                libelle = item.Element("Libelle").Value,
+                                patient = item.Element("PatientId").Value,
+                                dateHeureRDV = item.Element("DateRendezVous").Value,
+                            };
 
                 await DisplayAlert("Alert RDV", responseSOAP, "OK");
 
-               //this.listRDV.ItemsSource = query;
-            //}
-        }
+                this.listRDV.ItemsSource = query;
+            }
+        }        
 
         private void searchRDV_SearchButtonPressed(object sender, EventArgs e)
         {
-        chargerListeRDV(Convert.ToDateTime(this.searchRDV.Text));
+            //chargerListeRDV(Convert.ToDateTime(this.searchRDV.Text));
         }
 
         async private void lisRDV_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             await Navigation.PushModalAsync(new ConsultationPage((sender as ListView).SelectedItem as RDV), true);
+        }
+
+        private void btnResponseSoap_Clicked(object sender, EventArgs e)
+        {
+
+            chargerListeRDV("2018-03-16");
         }
     }
 }
