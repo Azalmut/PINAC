5498ecb35417a71a2abef769bc5da8bb6c2d46d3
Azalmut <Azalmut@vivaldi.net>
2018-03-16 17:06:04 +0100

    Add files via upload

---
+++ PINAC/CalculESC.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Score;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PINAC
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CalculESC : ContentPage
+	{
+		public CalculESC ()
+		{
+			InitializeComponent ();
+            this.entryTension.Text = this.stepTension.Value.ToString();
+            this.entryChol.Text = this.stepChol.Value.ToString();
+            this.entryAge.Text = this.stepAge.Value.ToString();
+        }
+
+        private void stepTension_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            this.entryTension.Text = this.stepTension.Value.ToString();
+            calcul();
+        }
+
+        private void stepChol_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            this.entryChol.Text = this.stepChol.Value.ToString();
+            calcul();
+        }
+
+        private void stepAge_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            this.entryAge.Text = this.stepAge.Value.ToString();
+            calcul();
+        }
+
+        public void calcul()
+        {
+            ScoreESC score = new ScoreESC();
+            int sexe;
+            int tabac;
+
+            if (this.switchSexe.IsToggled)
+                sexe = 1;
+            else
+                sexe = 2;
+
+            if (this.switchTabac.IsToggled)
+                tabac = 1;
+            else
+                tabac = 0;
+
+            this.lblScore.Text = "Score: " + score.calculateScoreCVD(sexe, tabac, Convert.ToInt32(this.stepAge.Value), Convert.ToInt32(this.stepTension.Value), Convert.ToInt32(this.stepChol.Value));
+        }
+
+        private void switchSexe_Toggled(object sender, ToggledEventArgs e)
+        {
+            calcul();
+        }
+
+        private void switchTabac_Toggled(object sender, ToggledEventArgs e)
+        {
+            calcul();
+        }
+    }
+}
