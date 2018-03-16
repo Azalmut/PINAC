5f768ff2973be45976b005cb5619a3323adbedc4
Azalmut <Azalmut@vivaldi.net>
2018-03-16 14:14:54 +0100

    Add files via upload

--- PINAC/ConsultationPage.xaml.cs
+++ PINAC/ConsultationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,28 +9,23 @@ using Xamarin.Forms.Xaml;
 
 namespace PINAC
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ConsultationPage : ContentPage
-    {
-        public ConsultationPage()
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ConsultationPage : ContentPage
+	{
+		public ConsultationPage()
+		{
+			InitializeComponent ();
+		}
+
+        public ConsultationPage(RDV leRendezVous)
         {
             InitializeComponent();
+            this.BindingContext = leRendezVous;
         }
 
-        public ConsultationPage(RDV leRDV)
-        {
-            InitializeComponent();
-            this.BindingContext = leRDV;
-        }
-
-        private void commencerConsultation_Clicked(object sender, System.EventArgs e)
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
         {
 
-        }
-
-        async private void retourAgenda_Clicked(object sender, System.EventArgs e)
-        {
-            await Navigation.PopModalAsync(true);
         }
     }
 }
